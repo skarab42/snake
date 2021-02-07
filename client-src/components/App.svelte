267c2d043e1ab2738d198ca809fd1a9b9bad5dc0
skarab42 <skarab@onlfait.ch>
2021-02-07 11:59:43 +0100

    feat: Add socket.io

--- client-src/components/App.svelte
+++ client-src/components/App.svelte
@@ -1,5 +1,16 @@
 <script>
   import Header from "./App/Header.svelte";
+  import io from "socket.io-client";
+
+  const socket = io();
+
+  socket.on("connect", () => {
+    console.log("connected");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
 </script>
 
 <div class="h-screen bg-gray-200 dark:bg-gray-900 dark:text-gray-200">
--- server/index.js
+++ server/index.js
@@ -1,14 +1,31 @@
 const { dev, host, port, name, version, clientPath } = require("./config");
+const socketIO = require("socket.io");
 const logger = require("./logger");
 const polka = require("polka");
 const sirv = require("sirv");
 
 const client = sirv(clientPath, { dev });
+const address = `http://${host}:${port}`;
 
-polka()
+const { server } = polka()
   .use(client)
   .listen(port, (err) => {
     if (err) throw err;
     logger.info(`${name} v${version}`);
-    logger.info(`running on http://${host}:${port}`);
+    logger.info(`running on ${address}`);
   });
+
+const io = socketIO(server, {
+  cors: {
+    origin: address,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+});
