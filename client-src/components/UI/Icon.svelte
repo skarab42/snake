fd5bea1a6467f2d2a780fdb5faa3ec5a7c82c51f
skarab42 <skarab@onlfait.ch>
2021-02-07 14:34:49 +0100

    feat: Add sizes

--- client-src/components/UI/Icon.svelte
+++ client-src/components/UI/Icon.svelte
@@ -1,7 +1,19 @@
 <script>
   export let icon;
+
+  export let size = "sm";
+
+  const sizes = {
+    xs: "w-4 h-4",
+    sm: "w-6 h-6",
+    md: "w-8 h-8",
+    lg: "w-10 h-10",
+    xl: "w-12 h-12",
+  };
+
+  let sizeClass = sizes[size] || sizes.sm;
 </script>
 
-<span {...$$restProps}>
+<span class="flex fill-current items-center inline-block {sizeClass}">
   <svelte:component this="{icon}" />
 </span>
