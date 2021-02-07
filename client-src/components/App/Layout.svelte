f07f9936c128d163f528f41781616e10c6db3d41
skarab42 <skarab@onlfait.ch>
2021-02-07 13:58:49 +0100

    feat: Add Layout component

---
+++ client-src/components/App/Layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Header from "@/components/App/Header.svelte";
+
+  export let title = null;
+</script>
+
+<div class="h-screen bg-gray-200 dark:bg-gray-900 dark:text-gray-200">
+  <Header title="{title}">
+    <slot name="header" />
+  </Header>
+  <slot />
+</div>
