e8d1a38de2ec312e33f88ce96cc5aeabe84a44bd
skarab42 <skarab@onlfait.ch>
2021-02-09 10:50:32 +0100

    feat: Start/stop game on first/last connexion

--- server/plugins/snake.js
+++ server/plugins/snake.js
@@ -6,11 +6,62 @@ let division = 10;
 let interval = 5000;
 
 let snake = null;
-let action = null;
+let action = "up";
+let actions = {};
+
+let clientsCount = 0;
+let tickIntervalId = null;
+
+function getAction() {
+  const newAction = Object.entries(actions).sort((a, b) => {
+    return b[1] - a[1];
+  })[0];
+
+  return newAction ? newAction[0] : action;
+}
 
 function runAction() {
-  console.log("run action:", action);
+  action = getAction();
+  logger.info(`snake: run action: ${action}`);
   snake.emit("action", { action });
+  actions = {};
+}
+
+function addAction(input) {
+  if (!actions[input]) {
+    actions[input] = 1;
+  } else {
+    actions[input]++;
+  }
+}
+
+function startGame() {
+  if (tickIntervalId) {
+    logger.warn("snake: already stared");
+    return false;
+  }
+  logger.warn("snake: start");
+  tickIntervalId = setInterval(() => {
+    const max = division - 1;
+    const tick = ticks % division;
+    const lastTick = max === tick;
+    const percent = Math.round((tick / max) * 100);
+    snake.emit("tick", { tick, percent, lastTick });
+    lastTick && runAction();
+    ticks++;
+  }, interval / division);
+  return true;
+}
+
+function stopGame() {
+  if (!tickIntervalId) {
+    logger.warn("snake: already stopped");
+    return false;
+  }
+  logger.warn("snake: stop");
+  clearInterval(tickIntervalId);
+  tickIntervalId = null;
+  return true;
 }
 
 function init() {
@@ -18,31 +69,27 @@ function init() {
   snake = io().of("/snake");
 
   snake.on("connection", (socket) => {
-    logger.info(`snake: user connected`);
+    clientsCount++;
+    logger.info(`snake: user connected`, { clientsCount });
+
+    !tickIntervalId && startGame();
 
     socket.on("disconnect", () => {
-      logger.info("snake: user disconnected");
+      clientsCount--;
+      logger.info("snake: user disconnected", { clientsCount });
+      if (clientsCount === 0) stopGame();
     });
 
     socket.on("keydown", (input) => {
       logger.info("snake: keydown", { input });
-      action = input;
+      addAction(input);
     });
   });
-
-  setInterval(() => {
-    const max = division - 1;
-    const tick = ticks % division;
-    const lastTick = max === tick;
-    const percent = Math.round((tick / max) * 100);
-    snake.emit("tick", { tick, percent, lastTick });
-    if (action && lastTick) runAction();
-    ticks++;
-  }, interval / division);
 
   return io;
 }
 
 module.exports = {
   init,
+  startGame,
 };
